9c71ff43ea98a8ba7378513bead89c147cd95df4
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-08-21 11:49:47 -0500

    NEW FILES ADDED (ARRAY MANIPULATION AS WELL AS SPLIT AND JOIN EXERCISE ).

---
+++ js/planets-string.js
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////// EXERCISE DEMONSTRATING THE USE OF SPLIT AND JOIN ////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray;
+
+    // TODO: Convert planetsString to an array, save it to planetsArray.
+    planetsArray = planetsString.split("|");
+
+    console.log(planetsArray);
+
+    // TODO: Create a string with <br> tags between each planet. console.log() your results.
+    planetsArray.join("<br>");
+    //       Why might this be useful?
+    console.log(planetsArray);
+    // Bonus: Create a second string that would display your planets in an undordered list.
+    //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
+    //        console.log() your results.
+    var bonus = ["<ul>",
+                        "<li>",
+                            "Mercury",
+                        "</li>",
+                        "<li>",
+                            "Venus",
+                        "</li>",
+                        "<li>",
+                            "Earth",
+                        "</li>",
+                        "<li>",
+                            "Mars",
+                        "</li>",
+                        "<li>",
+                            "Jupiter",
+                        "</li>",
+                        "<li>",
+                            "Saturn",
+                        "</li>",
+                        "<li>",
+                            "Uranus",
+                        "</li>",
+                        "<li>",
+                            "Neptune",
+                        "</li>",
+                "</ul>"];
+
+    console.log(bonus);
+})();
