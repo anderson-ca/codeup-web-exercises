d952f0930fe31daf8046523e622e09f2b7c64705
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-08-29 11:06:18 -0500

    FINISHED BASIC CALCULATOR MODEL.

--- js/calculator.js
+++ js/calculator.js
@@ -1,26 +1,47 @@
 "use strict";
 
-var box = document.getElementById("display");
-var x;
 
-function toScreen(x) {
-    box.value += x;
-    if (x === 'c') {
-        box.value = '';
+
+
+
+
+
+/////////////////////////////// VARIABLE DECLARATIONS.
+var container = document.getElementById("display");
+
+var container2 = document.getElementById("display2");
+
+var container3 = document.getElementById("display3");
+
+var numb;
+
+var empty = "";
+
+////////////////////////////// LOGICAL OPERATION DISPLAYING NUMBERS AND OPERATORS.
+function toScreen(numb) {
+    if (numb === "+" || numb === "-" || numb === "*" || numb === "/") {
+        container2.value = numb;
+    } else if (container2.value !== empty) {
+        container3.value += numb;
+    } else {
+        container.value += numb;
+    }
+
+////////////////////////////// CLEAR EVERYTHING.
+    if (numb === 'c') {
+        container.value = '';
+        container2.value = '';
+        container3.value = '';
     }
 }
 
+///////////////////////////// DISPLAY FINAL RESULT.
 function equalTo() {
-    x = box.value;
-    x = eval(x);
-    box.value = x;
+    numb = container.value;
+    numb = eval(numb + container2.value + container3.value);
+    container.value = numb;
+    container3.value = '';
 }
-
-
-
-
-
-
 
 
 
